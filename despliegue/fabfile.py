bc3cb9ea3c7124692b4e287d68c5e487d48ca7c0
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-12-26 12:38:21 +0100

    Actualizo despliegue

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -2,19 +2,13 @@
 # -*- coding: utf-8 -*-
 from fabric.api import * 
 
+# Funcion que se encarga de actualizar la aplicación con el nuevo contenido de mi repositorio
 def Actualizar():
-    # Borramos el contenido anterior
     run('sudo rm -rf ./IV-18-19-Proyecto')
-
-    # Creamos la carpeta
-    run('mkdir IV-18-19-Proyecto && cd IV-18-19-Proyecto')
-
-    # Actualizamos con la nueva versión del repositorio
     run('git clone https://github.com/Thejokeri/IV-18-19-Proyecto.git')
-
-    # Instalamos requirements
     run('pip3 install -r ./IV-18-19-Proyecto/requirements.txt')
 
+# Funcion que se encargar de Iniciar la aplicación
 def Iniciar():
-    # Iniciamos el servicio
-    run('cd IV-18-19-Proyecto && sudo gunicorn app:app -b 0.0.0.0:80')
+    run('cd IV-18-19-Proyecto')
+    run('sudo gunicorn app:app -b 0.0.0.0:80 &')
