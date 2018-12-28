a2e3287cd430822ab65e1b860b59581d48732a82
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-12-28 21:31:49 +0100

    A la tercera va la vencida

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -42,8 +42,7 @@ def Iniciar():
 # Funcion para mostrar el STATUS del despliegue
 def ShowDeploy():
     run('CURL=$(curl 0.0.0.0:80)')
-    run('export $CURL')
-    print(os.environ['CURL'])
+    run('echo $CURL')
 
 # Funcion que se encarga de parar el proceso de ejecución de la aplicación
 def Parar():
