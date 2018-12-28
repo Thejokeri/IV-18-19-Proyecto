430f000e3aff94c9f4bc06dec0e091b900ce83d3
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-12-28 21:53:51 +0100

    Despliegue

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -41,7 +41,8 @@ def Iniciar():
 
 # Funcion para mostrar el STATUS del despliegue
 def ShowDeploy():
-    run('CURL=$(curl 0.0.0.0:80) && sudo export CURL')
+    run('CURL=$(curl 0.0.0.0:80)')
+    run('sudo export CURL')
     print(os.environ['CURL'])
 
 # Funcion que se encarga de parar el proceso de ejecución de la aplicación
