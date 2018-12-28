59621bccddb53758943609e2812ce46c2067bc5c
Fernando Talavera Mendoza <ftm1997@correo.ugr.es>
2018-12-28 20:45:00 +0100

    Actualizo despliegue

--- despliegue/fabfile.py
+++ despliegue/fabfile.py
@@ -9,7 +9,8 @@ exists = os.path.isdir('./IV-18-19-Proyecto')
 # Funcion para instalar la aplicación en nuestra máquina
 def Instalar():
     if exists:
-        run('rm -rf ./IV-18-19-Proyecto')
+        print('Existe el archivo, lo borramos')
+        run('sudo rm -rf ./IV-18-19-Proyecto')
     else:
         run('git clone https://github.com/Thejokeri/IV-18-19-Proyecto.git')
         run('pip3 install -r ./IV-18-19-Proyecto/requirements.txt')
@@ -19,6 +20,7 @@ def Instalar():
 # Funcion que se encarga de actualizar la aplicación con el nuevo contenido de mi repositorio
 def Actualizar():
     if exists:
+        print('Existe el archivo, actualizamos la aplicación')
         run('cd ./IV-18-19-Proyecto && git pull')
         run('pip3 install -r ./IV-18-19-Proyecto/requirements.txt')
         run('pip install --upgrade pip')
@@ -29,6 +31,7 @@ def Actualizar():
 # Funcion que se encargar de Iniciar la aplicación
 def Iniciar():
     if exists:
+        print('Existe el archivo, iniciamos la aplicación')
         run('cd IV-18-19-Proyecto && sudo gunicorn app:app -b 0.0.0.0:80')
     else:
         print('Imposible arrancar la aplicación, debe de instalarla antes')
